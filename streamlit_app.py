0a8800401bdf2b353451de5e21c662c0e15baa9e
soroushpiri-glitch <soroush.piri@morgan.edu>
2026-04-27 12:52:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,4 @@
 import streamlit as st
-from npi_chatbot_sql import bedrock_agent
 
 st.title("NPI Agent")
-
-question = st.text_input("Ask about providers")
-
-if st.button("Submit"):
-    answer = bedrock_agent(question)
-    st.write(answer)
+st.write("Deployment works.")
